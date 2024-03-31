1dbc1a11a2084a36e2cee22cd648d5d4d2efacc6
lndsilva <lndsilva@hotmail.com>
2024-03-31 18:17:56 -0300

    template para telas stacks

---
+++ src/components/TextoCentral.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default props => (
+    <View style={
+        {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center'
+        }
+    }>
+        <Text style={{fontSize: 50}}>
+            {props.children}
+        </Text>
+    </View>
+)
